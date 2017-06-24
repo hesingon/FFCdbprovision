1e30236e9333123ede99ec72c461e3511e909810
He Xing Darius <E0007888@u.nus.edu>
2017-06-24 11:06:35 +0800

    try again 2

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var MongoClient = mongodb.MongoClient;
 // Connection URL. This is where your mongodb server is running.
 
 //(Focus on This Variable)
-var url = 'mongodb://admin:123456@ds139959.mlab.com:39959/url-shortener';     
+var url ='mongodb://heroku_nhvz97w1:a85dmnne2i516anqq867ir1b83@ds135552.mlab.com:35552/heroku_nhvz97w1'; 
+
+//'mongodb://admin:123456@ds139959.mlab.com:39959/url-shortener';     
 //(Focus on This Variable)
 
 // Use connect method to connect to the Server
